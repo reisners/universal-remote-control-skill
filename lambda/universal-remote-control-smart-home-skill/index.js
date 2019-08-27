85a0ebae17767f46a5a199d872493c469f223dd1
Dr. Stefan Reisner <stefan.reisner@springer.com>
2019-08-27 08:02:14 +0200

    renamed universion-remote-control-smart-home-skill (from universal-remote-control), added claudia.json, package-lock.json

--- lambda/universal-remote-control-smart-home-skill/index.js
+++ lambda/universal-remote-control-smart-home-skill/index.js
@@ -2,10 +2,13 @@ const { google } = require('googleapis');
 
 const GOOGLE_SHEET_NAME = "UniversalRemoteControlData";
 
-const DISPLAY_CATEGORIES = {
-    "Alexa.PowerControl": "SWITCH",
-    "Alexa.ChannelController": "TV"
-};
+const AWS = require('aws-sdk');
+
+AWS.config.update({region:'eu-west-1'});
+
+const ddb = new AWS.DynamoDB.DocumentClient();
+
+const { TABLE_NAME, ENDPOINT } = process.env;
 
 exports.handler = function (request, context) {
     if (request.directive.header.namespace === 'Alexa.Discovery' && request.directive.header.name === 'Discover') {
@@ -25,21 +28,35 @@ exports.handler = function (request, context) {
         let oAuth2Client = createOAuthClient(accessToken);
         let attributes = await retrieveAttributes(oAuth2Client);
         let devices = await retrieveDevices(attributes.spreadsheetId, oAuth2Client);
+        log("DEBUG:", "devices=", JSON.stringify(devices));
         
-        log("DEBUG:", "spreadsheetId=", attributes.spreadsheetId);
-
         var payload = {
             "endpoints": Object.entries(devices).map((entry) => {
 
-                let deviceName = entry[0];
-                let interface = Object.entries(entry[1])[0].category; // all rows for the same device must have identical category, so we pick the first one here
+                let endpointId = entry[0];
+                let device = entry[1];
+                let [urcid] = endpointId.split(":");
+                let operations = Object.entries(device);
+                let operation0 = operations[0][1];
+                let interface = operation0.category; // all rows for the same device must have identical URCID, Device, Display Category and Category values, so we pick the first one here
+                let displayCategory = operation0.displayCategory;
+                let deviceName = operation0.device;
                 return {
-                    "endpointId": "urc:"+deviceName,
+                    "endpointId": endpointId,
                     "manufacturerName": "Universal Remote Control",
                     "friendlyName": deviceName,
                     "description": deviceName,
-                    "displayCategories": [DISPLAY_CATEGORIES[interface]],
-                    "cookie": entry[1],
+                    "displayCategories": [displayCategory],
+                    "additionalAttributes": {
+                        "manufacturer" : "Universal Remote Control",
+                        "model" : "MK1",
+                        "serialNumber": urcid,
+                        "firmwareVersion" : "0.0",
+                        "softwareVersion": "0.0",
+                        "customIdentifier": urcid
+                    },
+                    "cookie": device,
+                    "connections": [],
                     "capabilities":
                     [
                         {
@@ -58,7 +75,7 @@ exports.handler = function (request, context) {
         };
         var header = request.directive.header;
         header.name = "Discover.Response";
-        log("DEBUG", "Discovery Response: ", JSON.stringify({ header: header, payload: payload }));
+        log("DEBUG:", "Discovery Response: ", JSON.stringify({ header: header, payload: payload }));
         context.succeed({ event: { header: header, payload: payload } });
     }
 
@@ -67,14 +84,22 @@ exports.handler = function (request, context) {
     }
 
     function handlePowerControl(request, context) {
-        // get device ID passed in during discovery
-        var requestMethod = request.directive.header.name;
-        var responseHeader = request.directive.header;
+        log("handlePowerController(request="+JSON.stringify(request, getCircularReplacer())+")")
+
+        let requestMethod = request.directive.header.name;
+        let responseHeader = request.directive.header;
         responseHeader.namespace = "Alexa";
         responseHeader.name = "Response";
         responseHeader.messageId = responseHeader.messageId + "-R";
         // get user token pass in request
-        var requestToken = request.directive.endpoint.scope.token;
+        let requestToken = request.directive.endpoint.scope.token;
+
+        let endpointId = request.directive.endpoint.endpointId;
+        let cookie = request.directive.endpoint.cookie;
+
+        //TODO: find entry in Google sheet matching endpointId=URCID:Device & requestMethod=Operation
+        //TODO: make URC return the powerResult value ("ON"/"OFF") and pass it back to Alexa
+
         var powerResult;
 
         if (requestMethod === "TurnOn") {
@@ -151,7 +176,7 @@ exports.handler = function (request, context) {
     async function retrieveDevices(spreadsheetId, oAuth2Client) {
         let request = {
             spreadsheetId: spreadsheetId,
-            range: 'A2:D',
+            range: 'A2:G',
             valueRenderOption: 'UNFORMATTED_VALUE',
             dateTimeRenderOption: 'FORMATTED_STRING',
             auth: oAuth2Client,
@@ -161,21 +186,26 @@ exports.handler = function (request, context) {
             const sheets = google.sheets('v4');
             sheets.spreadsheets.values.get(request, function(err, response) {
                 if (err) {
-                    console.log('an error occured');
                     console.error(err);
                     reject(err);
                 } else {
                     let devices = {};
-                    //console.log('read spreadsheet: '+JSON.stringify(response, getCircularReplacer()));
+                    log("DEBUG:", 'read spreadsheet: '+JSON.stringify(response, getCircularReplacer()));
                     response.data.values.forEach((row) => {
-                        let [device, category, operation, channel, data] = row;
-                        if (!devices[device]) {
-                            devices[device] = {};
+                        try {
+                            let [urcid, device, displayCategory, category, operation, channel, jsonData] = row;
+                            let endpointId = urcid + ":" + device;
+                            let data = JSON.parse(jsonData);
+                            if (!devices[endpointId]) {
+                                devices[endpointId] = {};
+                            }
+                            if (devices[endpointId][operation]) {
+                                console.log("WARNING: duplicate entries for endpointId "+endpointId+" operation "+operation);
+                            }
+                            devices[endpointId][operation] = { urcid: urcid, device: device, displayCategory: displayCategory, category: category, channel: channel, data: data };
+                        } catch (e) {
+                            reject(e);
                         }
-                        if (devices[device][operation]) {
-                            console.log("WARNING: duplicate entries for device "+device+" operation "+operation);
-                        }
-                        devices[device][operation] = { category: category, channel: channel, data: data };
                     });
                     console.log('read devices: '+JSON.stringify(devices, getCircularReplacer()));
                     resolve(devices);
@@ -184,6 +214,48 @@ exports.handler = function (request, context) {
         });    
     }
     
+    async function executeCommmand(urcid, channel, parsedData) {
+
+        connectionData = await ddb.scan({ TableName: TABLE_NAME, ProjectionExpression: '#connectionId',
+                                        ExpressionAttributeNames: {
+                                            '#URCID': 'URCID',
+                                            '#connectionId': 'connectionId'
+                                        },
+                                        ExpressionAttributeValues: {
+                                        ":urcid": urcid
+                                        }, 
+                                        FilterExpression: "#URCID = :urcid", 
+                                        }).promise();
+        const apigwParams = {
+            apiVersion: '2018-11-29',
+            endpoint: ENDPOINT
+        };
+        
+        console.log("API Gateway Parameters: "+JSON.stringify(apigwParams));
+        
+        const apigwManagementApi = new AWS.ApiGatewayManagementApi(apigwParams);
+                
+        const payload = { command: "SEND", channel: channel, data: parsedData };
+        const payloadJson = JSON.stringify(payload);
+        
+        const postCalls = connectionData.Items.map(async ({ connectionId }) => {
+            console.log("posting "+payloadJson+" to connectionId "+connectionId);
+            try {
+                const res = await apigwManagementApi.postToConnection({ ConnectionId: connectionId, Data: payloadJson}).promise();
+                console.log("res = "+res);
+            } catch (e) {
+                console.log("error: "+JSON.stringify(e));
+                if (e.statusCode === 410) {
+                    console.log(`Found stale connection, deleting ${connectionId}`);
+                    await ddb.delete({ TableName: TABLE_NAME, Key: { connectionId } }).promise();
+                } else {
+                    throw e
+                }
+            }
+        });
+        
+        await Promise.all(postCalls);
+    }
 }
 
 const getCircularReplacer = () => {
@@ -198,3 +270,5 @@ const getCircularReplacer = () => {
       return value;
     };
   };
+
+  
